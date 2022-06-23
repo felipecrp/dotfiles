d6ed5787b990c8841a6064360ba9d7c05d1fb608
Felipe Curty <felipecrp@gmail.com>
2022-06-22 23:32:05 -0300

    Update config to run with WSL

--- nvim/lua/code.lua
+++ nvim/lua/code.lua
@@ -209,7 +209,7 @@ cmp.setup.cmdline(':', {
     sources = cmp.config.sources({
         { name = 'path' }
     }, {
-        { name = 'cmdline' }
+        { name = 'cmdline', keyword_pattern=[=[[^[:blank:]\!]*]=], keyword_length=3 }
     })
 })
 
