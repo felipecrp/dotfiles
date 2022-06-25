94cfc783d636dd4713b2158f5115a5dd42035770
Felipe Curty <felipecrp@gmail.com>
2022-06-25 13:53:57 -0300

    Add keybind to quickly switch buffers

--- nvim/lua/ui.lua
+++ nvim/lua/ui.lua
@@ -18,3 +18,7 @@ vim.cmd 'colorscheme gruvbox'
 
 vim.cmd[[ highlight WinSeparator guibg=None ]]
 vim.opt.laststatus = 3
+
+vim.api.nvim_set_keymap('n', '<Tab>', ':bn<CR>', { noremap = true } )
+vim.api.nvim_set_keymap('n', '<S-Tab>', ':bn<CR>', { noremap = true } )
+
